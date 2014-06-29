34941821b6772830016b6095418836c7e096f2e9
xstyr <dr573@york.ac.uk>
2014-06-29 22:47:20 +0100

    Initial Commit

---
+++ Markov Chain Text/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Markov Chain Text
+//
+//  Created by Mac on 29/06/2014.
+//  Copyright (c) 2014 xstyr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
